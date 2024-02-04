e19ad51f41274f60b39ce8a034c0defcf5e57284
JinHee <327jhl@naver.com>
2024-02-05 00:50:27 +0900

    add: 절대 경로 설정 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@types': './src/types',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+        },
+      },
+    ],
+  ],
 };
