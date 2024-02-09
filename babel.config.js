6f57ff4409af5ef8f5b4ca3b94fca508472fb24c
JinHee <327jhl@naver.com>
2024-02-09 16:19:10 +0900

    change: 절대 경로에서 extension auto import 기능 제거

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,6 @@ module.exports = {
         root: ['./src'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
-          '@': './src',
           '@assets': './src/assets',
           '@components': './src/components',
           '@hooks': './src/hooks',
