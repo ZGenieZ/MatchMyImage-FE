27dce8eec9c92769f2a282ca493a6c65c687aec6
이진희 <327jhl@naver.com>
2024-02-12 16:53:22 +0900

    [TAS-43] add: 카카오 로그인 토큰 발급 로직 구현 (#1)
    * [TAS-43] add: 카카오 로그인, config 패키지 설치, env 타입 선언
    
    * [TAS-43] add: AOS 카카오 로그인 로직 구현
    
    * [TAS-43] add: IOS 카카오 로그인 로직 구현

--- App.tsx
+++ App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Button, SafeAreaView } from 'react-native';
+import { Alert, Button, SafeAreaView } from 'react-native';
+
+import { login } from '@react-native-seoul/kakao-login';
+
+const signInWithKakao = async () => {
+  return await login()
+    .then(result => {
+      console.log('result: ', result);
+      Alert.alert('result', JSON.stringify(result, null, 2));
+      return result;
+    })
+    .catch(error => {
+      console.log('Error: ', error);
+      throw error;
+    });
+};
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <Button title="카카오 로그인" />
-      <Button
-        title="구글 로그인(구현 예정)"
-        onPress={() => {
-          console.log('구글');
-        }}
-      />
+      <Button title="카카오 로그인" onPress={signInWithKakao} />
+      <Button title="구글 로그인(구현 예정)" />
       <Button title="애플 로그인(구현 예정)" />
     </SafeAreaView>
   );
---
+++ react-native-config.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-native-config' {
+  export const KAKAO_APP_KEY: string;
+  export const KAKAO_APP_KEY_WITH_KAKAO: string;
+}
