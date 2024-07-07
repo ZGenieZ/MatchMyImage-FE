ae3652082cfa9f85da56ef5ecac7eb6aebca1842
JinHee <327jhl@naver.com>
2024-07-07 22:32:00 +0900

    [TAS-132] add: axios request interceptor 로직 추가

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,11 +1,26 @@
 import axios from 'axios';
+import UserAgent from 'react-native-user-agent';
+
+import { useAuthStore } from 'stores/auth';
 
 const BASE_URL = 'http://localhost:8080/api';
 
 const apiClient = axios.create({ baseURL: BASE_URL });
 
 apiClient.interceptors.request.use(config => {
-  return config;
+  const {
+    tokenInfo: { access, signup },
+    isLoggedIn,
+    isNeedSignUp,
+  } = useAuthStore();
+
+  const Authorization = `Bearer ${isNeedSignUp ? signup?.token : access?.token}`;
+
+  return {
+    ...config,
+    ...(isLoggedIn && { Authorization }),
+    userAgent: UserAgent.getUserAgent(),
+  };
 });
 
 apiClient.interceptors.response.use(response => {
